b73f087489e884883cd4f977f1a5ef7325509c24
Krystal <krystal@MyMacBookde-MacBook-Pro.local>
2018-05-23 23:44:09 +0800

    Commit_05
    upadte member part

--- member/views.py
+++ member/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'member/index.html',locals())
+
+def signup(request):
+    return render(request,'member/signup.html',locals())
