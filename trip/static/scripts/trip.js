b28961de6908d882113cb40de00a45b7d1d128c0
Su Wei-Chu <su@Sus-MacBook-Air.local>
2018-06-06 13:17:19 +0800

    trip update panel display

--- trip/static/scripts/trip.js
+++ trip/static/scripts/trip.js
@@ -31,7 +31,7 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if (panel.style.display == "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
