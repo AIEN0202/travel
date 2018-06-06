ef11b791b42e3aef52e95fe39250edc63aeb9d4c
蔡松峻 <geraldss74@gmail.com>
2018-06-06 16:46:44 +0800

    COMMIT 33
    merger hotel

--- hotel/modelsres.py
+++ hotel/modelsres.py
@@ -9,27 +9,60 @@ class bookingq:
     
     def simg(self, id):
         with connection.cursor() as cursor:
-            cursor.execute("select imgsrc from restaurant where resid=%s",(id,))
+            cursor.execute("select imgsrc from hotel where id_hotel=%s",(id,))
             data = cursor.fetchone()
         return data
 
     def detail(self, id):
         with connection.cursor() as cursor:
-            cursor.execute("select title,type,tel from restaurant where resid=%s",(id,))
+            cursor.execute("select title,type from hotel where id_hotel=%s",(id,))
             data1 = cursor.fetchone()
         return data1
+    
+
+    def Hname(self, id):
+        with connection.cursor() as cursor:
+            cursor.execute("select title from hotel where id_hotel=%s",(id,))
+            data5 = cursor.fetchone()
+        return data5
+
+    
 
     def ad(self, id):
         with connection.cursor() as cursor:
-            cursor.execute("select time,addr from restaurant where resid=%s",(id,))
+            cursor.execute("select style,style1,h_price,h_price1 from travel.hotel where id_hotel=%s",(id,))
             data2 = cursor.fetchone()
         return data2
 
-    def resid(self):
+    def id_hotel(self):
         with connection.cursor() as cursor:
             cursor.execute("select TripID from itinerary")
             data3 = cursor.fetchone()
         return data3
+    def atdid(self):
+        with connection.cursor() as cursor:
+            cursor.execute("select Attracid from itinerary_day where idday = %s",(day,))
+            data6 = cursor.fetchone()
+        return data6
+
+    def hotelid(self,day):
+        with connection.cursor() as cursor:
+            cursor.execute("select Hotno from itinerary_day where idday = %s",(day,))
+            data6 = cursor.fetchone()
+        return data6
+
+    def itine(self,id):
+        with connection.cursor() as cursor:
+            cursor.execute("select * from itinerary where idMember = %s",(id,))
+            data6 = cursor.fetchone()
+        return data6
+
+    def trip(self, id):
+        with connection.cursor() as cursor:
+            cursor.execute("select * from travel.itinerary as A,travel.itinerary_day as B where A.TripID = B.TripID and A.TripID =%s",(id,))
+            data4 = cursor.fetchall()
+        return data4       
+
 
     def create(self, bk):
         with connection.cursor() as cursor:
@@ -47,3 +80,16 @@ class bookingq:
         with connection.cursor() as cursor:
             sql = "delete from products where productid=%s"
             cursor.execute(sql,(id,))
+
+    def country(self,id):
+        with connection.cursor() as cursor:
+            cursor.execute("select Countryname from country where idCountry = %s",(id,))
+            data8 = cursor.fetchone()
+        return data8
+
+    def area(self,id):
+        with connection.cursor() as cursor:
+            cursor.execute("select Regionname from region where idRegion = %s",(id,))
+            data9 = cursor.fetchone()
+        return data9 
+
